77e688700bb7a82a936f0aee2997640b58d86e57
Thiago Farias <tfarias@protonmail.com>
2026-01-05 09:54:50 -0300

    fix aggregate putEvent

--- src/mixins/aggregate.ts
+++ src/mixins/aggregate.ts
@@ -2,7 +2,7 @@ import { Describable, describedBy } from "../utils/describers";
 import { removeFunctions } from "../utils/remove-fn";
 
 export type Copy<T> = { -readonly [P in keyof T]: Copy<T[P]> };
-export type BaseEvent<Key extends PropertyKey, T = unknown> = Readonly<{ eventType: Key; data: Readonly<T> }>;
+export type BaseEvent<Key extends PropertyKey, T = unknown> = { eventType: Key; data: T };
 
 export interface Applier<T = any, E = any> {
   (currentValue: Copy<T>, event: Readonly<E>): Readonly<T>;
@@ -11,7 +11,7 @@ export interface Applier<T = any, E = any> {
 export type Aggregate<T, eventKey extends PropertyKey, E> = T & {
   commit(): Aggregate<T, eventKey, E>;
   fold<B>(fn: (agg: Aggregate<T, eventKey, E>) => B): B;
-  putEvent<K extends eventKey>(event: BaseEvent<K, E extends any ? E : never>): Aggregate<T, eventKey, E>;
+  putEvent(event: E): Aggregate<T, eventKey, E>;
   peekChanges(): readonly BaseEvent<eventKey, E>[];
 } & Describable;
 
@@ -19,19 +19,21 @@ export const aggregateRoot = <A extends Record<PropertyKey, Applier>>(appliers: 
   type Key = keyof A;
   type T = A[Key] extends Applier<infer TT extends object, any> ? TT : object;
   type DataFor<K extends Key> = A[K] extends Applier<T, infer D> ? D : never;
-  type EventFor<K extends Key> = Readonly<{ eventType: K; data: Readonly<DataFor<K>> }>;
+  type EventFor<K extends Key> = { eventType: K; data: DataFor<K> };
   type EventUnion = { [K in Key]: EventFor<K> }[Key];
   type Agg = Aggregate<T, Key, EventUnion>;
 
-  const aggregatePrototype = (initialData?: T, uncommitedChanges: readonly EventUnion[] = []): Agg => {
+  const aggregatePrototype = (initialData?: T, uncommitedChanges: EventUnion[] = []): Agg => {
     const data: T = uncommitedChanges.reduce(
       (acc, e) => appliers[e.eventType as Key](structuredClone(acc) as Copy<T>, e.data),
       removeFunctions(initialData ?? {})
     );
 
+    const putEvent = <K extends Key>(event: EventFor<K>) => aggregatePrototype(initialData, [...uncommitedChanges, event]);
+
     return {
       ...data,
-      putEvent: (event) => aggregatePrototype(initialData, [...uncommitedChanges, event] as readonly EventUnion[]),
+      putEvent,
       peekChanges: () => uncommitedChanges,
       commit: () => aggregatePrototype(data, []),
       fold: (fn) => fn(aggregatePrototype(data, uncommitedChanges)),
